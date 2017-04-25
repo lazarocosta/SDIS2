384fd3e70e1791a223ee1c30b806a7c35c563097
Telmo Barros <telmo.20@hotmail.com>
2017-04-25 16:25:31 +0100

    script for unix and windows

--- peer.sh
+++
@@ -1,3 +0,0 @@
-#start client
-echo "Starting peer..."
-java -classpath ./bin server.main.Peer 1.0 1 peer1 224.0.0.1 9001 224.0.0.2 9002 224.0.0.3 9003
--- run.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ./bin
-javac -d "bin" -cp "lib/*" ./src/gui/*.java
-java -cp "bin;lib/*" gui.Login
