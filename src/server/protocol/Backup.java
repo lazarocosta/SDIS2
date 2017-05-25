9db5c1fc4f8362e16dc7f289829ff4176436ce12
lazarocosta <lazaroosta@hotmail.com>
2017-05-25 14:30:00 +0100

    update

--- src/server/protocol/Backup.java
+++ src/server/protocol/Backup.java
@@ -12,72 +12,84 @@ import server.task.initiatorPeer.Delete;
 import server.task.initiatorPeer.PutChunk;
 
 public class Backup {
+    public int MAX_SIZE_CHUNK = 64 * 1000;
 
-	public Backup(String protocolVersion, String filePath, int replicationDeg) {
 
-		File f = new File(filePath);
-		long fileLength = f.length();
-		byte[] chunk = new byte[64000];
+    public Backup(String protocolVersion, String filePath, int replicationDeg) {
 
-		//get File ID
-		String fileID = Utils.getFileID(filePath);
-		String lastFileID = Peer.mdMap.get(filePath);
-		if(lastFileID != null && lastFileID.equals(fileID)){
-		}else{
-			if(lastFileID != null && !lastFileID.equals(fileID)){
-				new Thread(new Delete(protocolVersion, lastFileID)).start();
-			}
-			if(protocolVersion.equals("2.0")){
-				Peer.deletedFiles.remove(fileID); // remove from deleted files list
-				for(String delFileID: Peer.deletedFiles){
-					new Thread(new Delete(protocolVersion, delFileID)).start();
-				}
-			}
-			Peer.mdMap.put(filePath,fileID);
-			Utils.writeMD();
 
-		try {
-			InputStream in = new FileInputStream(f);
+        File f = new File(filePath);
+        long fileLength = f.length();
+        byte[] chunk = new byte[MAX_SIZE_CHUNK];
 
-			//Number of Chunks needed for the file
-			int numChunks = (int)(fileLength/64000)+1;
-			//Size of the last chunk
-			int lastChunkSize = (int)(fileLength%64000);
+        //get File ID
+        String fileID = Utils.getFileID(filePath);
+        String lastFileID = Peer.mdMap.get(filePath);
+        if (lastFileID != null && lastFileID.equals(fileID)) {
+        } else {
+            if (lastFileID != null && !lastFileID.equals(fileID)) {
+                new Thread(new Delete(protocolVersion, lastFileID)).start();
+            }
+            if (protocolVersion.equals("2.0")) {
+                Peer.deletedFiles.remove(fileID); // remove from deleted files list
+                for (String delFileID : Peer.deletedFiles) {
+                    new Thread(new Delete(protocolVersion, delFileID)).start();
+                }
+            }
+            Peer.mdMap.put(filePath, fileID);
+            Utils.writeMD();
 
-			//Create chunks and call PutChunks threads
-			for(int i = 0;i < numChunks;i++){
-				int numbytesRead;
-				//Special case 'Last Chunk'
-				if(i == numChunks-1){
-					chunk = new byte[lastChunkSize];
-					//Only reads if the size of the last Chunk is not 0, special case from multiples of 64000 in the file sizes
-					if(lastChunkSize != 0){
-						numbytesRead = in.read(chunk, 0, lastChunkSize);
-					}
-				}else numbytesRead = in.read(chunk, 0, 64000);
+            try {
+                InputStream in = new FileInputStream(f);
 
-				Thread putChunkThread = new Thread(new PutChunk(
-						protocolVersion,
-						fileID,
-					    i,
-					    replicationDeg,
-					    chunk
-					    ));
-				putChunkThread.start();
-				try {
-					putChunkThread.join();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+                //Number of Chunks needed for the file
+                int numChunks = (int) (fileLength / MAX_SIZE_CHUNK) + 1;
+                //Size of the last chunk
+                int lastChunkSize = (int) (fileLength % MAX_SIZE_CHUNK);
 
-			in.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-}
+                //Create chunks and call PutChunks threads
+                for (int i = 0; i < numChunks; i++) {
+                    int numbytesRead;
+                    //Special case 'Last Chunk'
+                    if (i == numChunks - 1) {
+                        chunk = new byte[lastChunkSize];
+                        //Only reads if the size of the last Chunk is not 0, special case from multiples of 64000 in the file sizes
+                        if (lastChunkSize != 0) {
+                            numbytesRead = in.read(chunk, 0, lastChunkSize);
+                        } else
+                            numbytesRead = 0;
+                    } else
+                        numbytesRead = in.read(chunk, 0, MAX_SIZE_CHUNK);
+
+                    int sizeEncrypted = (numbytesRead / 16 + 1) * 16;// https://stackoverflow.com/questions/3283787/size-of-data-after-aes-cbc-and-aes-ecb-encryption
+
+
+                    byte[] chunkEncrypted = new byte[sizeEncrypted];
+
+                   // chunkEncrypted =
+
+                    Thread putChunkThread = new Thread(new PutChunk(
+                            protocolVersion,
+                            fileID,
+                            i,
+                            replicationDeg,
+                            chunkEncrypted
+                    ));
+                    putChunkThread.start();
+                    try {
+                        putChunkThread.join();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                in.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
