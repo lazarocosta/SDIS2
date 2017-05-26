c3f195c940736c1b5a06637b96deb2ade6f00efe
José Aleixo <josealeixo.pc@gmail.com>
2017-05-26 16:33:10 +0100

    Created Sockets that only communicate if they have the same certificates.

---
+++ src/security/TLS.java
@@ -0,0 +1,86 @@
+package security;
+
+import javax.net.ssl.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TLS {
+
+    public static SSLContext sslContext = null;
+
+    public static SSLContext createSSLContext(){
+
+        try {
+            //configure SSL
+            char[] password = "123456".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            KeyStore ts = KeyStore.getInstance("JKS");
+
+            File keyFile = new File("./certificates/server.keys");
+            ks.load(new FileInputStream(keyFile), password);
+
+            File trustStoreFile = new File("./certificates/truststore");
+            ts.load(new FileInputStream(trustStoreFile), password);
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ts);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");  // create SSL context
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+            TLS.sslContext =  sslContext;
+
+            return sslContext;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+            return null;
+        }
+
+    }
+
+    public static ServerSocket createServerSocketFromContext(int port){
+
+        if(TLS.sslContext == null){
+            createSSLContext();
+        }
+
+        try {
+            return TLS.sslContext.getServerSocketFactory().createServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static Socket createClientSocketFromContext(String host, int port){
+
+        if(TLS.sslContext == null){
+            createSSLContext();
+        }
+
+        try {
+            return TLS.sslContext.getSocketFactory().createSocket(host, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    // For testing
+    public static void main(String[] args) {
+
+
+    }
+}
