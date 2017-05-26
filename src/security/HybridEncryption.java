04258a1bcdca15245f856afc68ec1d6a79b38d83
lazarocosta <lazaroosta@hotmail.com>
2017-05-26 19:05:09 +0100

    encrypted chunks and start saving keys in file

--- src/security/HybridEncryption.java
+++ src/security/HybridEncryption.java
@@ -1,22 +1,21 @@
 package security;
 
-import java.io.UnsupportedEncodingException;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import java.io.*;
+import java.security.*;
 
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-
+//KeyPair(PublicKey publicKey, PrivateKey privateKey)
 public class HybridEncryption {
 
-    private KeyPair asymmetricKeyPair;
+    static KeyPair asymmetricKeyPair;
     private SecretKeySpec symmetricKey;
     private IvParameterSpec iv;
-    public static String pathKey = "KEYS";
+    public static String pathKeyPublic = "KEY_PUBLIC.key";
+    public static String pathKeyPublicasymetric = "KEY_PUBLIC_ASY.key";
+
 
     public HybridEncryption() {
         try {
@@ -28,33 +27,33 @@ public class HybridEncryption {
         }
     }
 
-    public HybridEncryption(KeyPair asymmetricKeyPair, SecretKeySpec symmetricKey, IvParameterSpec iv){
+    public HybridEncryption(KeyPair asymmetricKeyPair, SecretKeySpec symmetricKey, IvParameterSpec iv) {
         this.asymmetricKeyPair = asymmetricKeyPair;
         this.symmetricKey = symmetricKey;
         this.iv = iv;
     }
 
-    public byte[] encrypt(byte[] value){
+    public byte[] encrypt(byte[] value) {
         return Encryptor.encryptAES(this.symmetricKey, this.iv, value);
     }
 
-    public byte[] decrypt(byte[] encrypted){
+    public byte[] decrypt(byte[] encrypted) {
         return Encryptor.decryptAES(this.symmetricKey, this.iv, encrypted);
     }
 
-    public byte[] encryptedSymmetricKey(){
+    public byte[] encryptedSymmetricKey() {
         return Encryptor.encryptRSA(this.asymmetricKeyPair.getPublic(), symmetricKey.getEncoded());
     }
 
-    public void decryptSymmetricKey(byte[] key){
+    public void decryptSymmetricKey(byte[] key) {
         this.symmetricKey = new SecretKeySpec(Encryptor.decryptRSA(this.asymmetricKeyPair.getPrivate(), key), "AES");
     }
 
-    public Key getAsymmetricPublicKey(){
+    public Key getAsymmetricPublicKey() {
         return asymmetricKeyPair.getPublic();
     }
 
-    public Key getAsymmetricPrivateKey(){
+    public Key getAsymmetricPrivateKey() {
         return asymmetricKeyPair.getPrivate();
     }
 
@@ -70,13 +69,39 @@ public class HybridEncryption {
         return iv;
     }
 
-    public void saveKeysFile(){
+    public void saveKeysFile() {
 
+        // Save public key
+        ObjectOutputStream chavePublicaOS = null;
+        try {
+            chavePublicaOS = new ObjectOutputStream(
+                    new FileOutputStream(pathKeyPublic));
 
+            chavePublicaOS.writeObject(asymmetricKeyPair.getPublic());
+            chavePublicaOS.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 
-    public void loadKeysFile (){
 
+    public PublicKey loadKeysFile() {
+
+        PublicKey publicKey = null;
+
+
+        ObjectInputStream inputStream = null;
+        try {
+            inputStream = new ObjectInputStream(new FileInputStream(pathKeyPublic));
+
+            publicKey = (PublicKey) inputStream.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
     }
 
     //FOR TESTING
