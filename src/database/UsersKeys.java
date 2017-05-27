86cad20db29261b2138220d0ea1854c99be70637
lazarocosta <lazaroosta@hotmail.com>
2017-05-27 21:33:17 +0100

    userKeys in databse

--- src/database/UsersKeys.java
+++ src/database/UsersKeys.java
@@ -6,11 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
-public class usersKeys {
+public class UsersKeys {
 
-    public static boolean insertUserKey(Connection c, int iduser, String key) throws SQLException {
+    public static boolean insertUserKey(Connection c, int iduser, byte[] key) throws SQLException {
         PreparedStatement preparedStatement = c.prepareStatement("INSERT INTO p2p.usersKeys(key, user_id) VALUES (?, ?)");
-        preparedStatement.setString(1, key);
+        preparedStatement.setBytes(1, key);
 
         preparedStatement.setInt(2, iduser);
         preparedStatement.executeUpdate();
@@ -18,14 +18,14 @@ public class usersKeys {
         return true;
     }
 
-    public static String loadUserKey(Connection c, int iduser) throws SQLException {
+    public static byte[] loadUserKey(Connection c, int iduser) throws SQLException {
         PreparedStatement preparedStatement = c.prepareStatement("SELECT * FROM p2p.usersKeys");
         ResultSet rs = preparedStatement.executeQuery();
 
         if(!rs.next())
             return null;
         else
-           return  rs.getString("key");
+           return  rs.getBytes("key");
 
     }
 
